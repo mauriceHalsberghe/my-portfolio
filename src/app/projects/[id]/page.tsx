31fe7b4c93347954a3d83bae7c6808dfb2f2f536
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-10-11 02:15:43 +0200

    Added project page styling and data

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,10 +1,39 @@
-export default async function ProjectPage({ 
-  params,
-}: { 
-  params: Promise<{id: string}>;
-}) {
+import projects from "../../data/projects.json";
+import Navbar from "../../components/Navbar";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../../ui/project.module.css"
 
-  const {id} = await params
+export default function ProjectPage({ params }: { params: { id: string } }) {
+  const { id } = params;
 
-  return <div>dit is project {id}</div>
+  const project = projects.find((p) => p.id.toString() === id);
+
+  if (!project) {
+    return (
+
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>
+          Project not found
+        </h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <Link href="/#hero">Go Back</Link>
+        </div>
+    )
+  }
+
+  return (
+    <>
+      <Navbar />
+      <section className={styles.project}>
+        <div className={styles.project__intro}>
+          <div>
+            <h1 className={styles.project__title}>{project.name}</h1>
+            <p>{project.description[0]}</p>
+          </div>
+          <Image className={styles.project__image} src={project.banner_img} width={600} height={0} alt={project.name} />
+        </div>
+      </section>
+    </>
+  );
 }
