52b993878c77942e3d024039e95ede84c110a1c4
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-10-13 01:38:00 +0200

    Added simple contact component

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,7 +1,34 @@
+import styles from "../ui/contact.module.css";
+import Link from "next/link";
+
 export default function Contact() {
-    return (
-        <section id="contact">
-            <h2>Contact</h2>
-        </section>
-    )
+  return (
+    <section className={styles.contact} id="contact">
+        <div className={styles.contact__content}>
+            <h2 className={styles.contact__title}>Contact</h2>
+            
+            <div className={styles.contact__links}>
+                <p className={styles.contact__link}>
+                <Link href="https://github.com/mauriceHalsberghe" target="_blank" rel="noopener noreferrer">
+                    GitHub
+                </Link>
+                </p>
+                <p className={styles.contact__link}>
+                <Link href="https://www.linkedin.com/in/maurice-halsberghe/" target="_blank" rel="noopener noreferrer">
+                    LinkedIn
+                </Link>
+                </p>
+                <p className={styles.contact__link}>
+                <Link href="mailto:maurice.halsberghe@student.arteveldehs.be">
+                    Email
+                </Link>
+                </p>
+            </div>
+        </div>
+
+      <p className={styles.contact__footer}>
+        © {new Date().getFullYear()} Maurice Halsberghe
+      </p>
+    </section>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
 
 export default async function Home() {
   return (
@@ -10,6 +12,7 @@ export default async function Home() {
       <Hero />
       <About />
       <Projects />
+      <Contact />
     </main>
   );
 }
