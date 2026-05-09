d19083cf0de4a00cc7867e7e00114ea8a92930ce
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2026-05-09 22:24:03 +0200

    remove unused files

--- src/components/icons/Burger.tsx
+++
@@ -1,9 +0,0 @@
-export function Burger({ className }: { className?: string }) {
-  return (
-    <svg width="24" height="24" viewBox="0 0 24 24" className={className}>
-      <g stroke="var(--text)" strokeLinecap="round" strokeWidth="2">
-        <path d="M4 18h16M4 12h16M4 6h16"/>
-      </g>
-    </svg>
-  );
-}
